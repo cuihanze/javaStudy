5499ace7d6b3c5451933cc5a5fa7f6bccae0a74c
cuihanze <cuihanze@meituan.com>
2019-09-24 15:21:53 +0800

    netty demo

---
+++ src/main/java/com/cui/study/netty/im/netty/NettyClient.java
@@ -0,0 +1,32 @@
+package com.cui.study.netty.im.netty;
+
+
+import io.netty.bootstrap.Bootstrap;
+import io.netty.channel.Channel;
+import io.netty.channel.ChannelInitializer;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.nio.NioSocketChannel;
+import io.netty.handler.codec.string.StringEncoder;
+public class NettyClient {
+    public static void main(String[] args) {
+        Bootstrap bootstrap = new Bootstrap();
+
+        NioEventLoopGroup group = new NioEventLoopGroup();
+
+        bootstrap
+                .group(group)
+                .channel(NioSocketChannel.class)
+                .handler(new ChannelInitializer<Channel>() {
+                    @Override
+                    protected void initChannel(Channel channel) throws Exception {
+                        channel.pipeline().addLast(new StringEncoder());
+                    }
+                });
+        Channel channel = bootstrap.connect("127.0.0.1", 8000).channel();
+
+        for (int i = 0; i < 100; i++) {
+            String content = "content " + i;
+            channel.writeAndFlush(content);
+        }
+    }
+}
---
+++ src/main/java/com/cui/study/netty/im/netty/NettyServer.java
@@ -0,0 +1,37 @@
+package com.cui.study.netty.im.netty;
+
+import io.netty.bootstrap.ServerBootstrap;
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.channel.ChannelInitializer;
+import io.netty.channel.SimpleChannelInboundHandler;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.nio.NioServerSocketChannel;
+import io.netty.channel.socket.nio.NioSocketChannel;
+import io.netty.handler.codec.string.StringDecoder;
+
+public class NettyServer {
+    public static void main(String[] args) {
+        ServerBootstrap serverBootstrap = new ServerBootstrap();
+
+        NioEventLoopGroup boss = new NioEventLoopGroup();// 接收新连接的线程
+        NioEventLoopGroup worker = new NioEventLoopGroup();// 负责读取数据的线程
+
+        serverBootstrap
+                .group(boss, worker)
+                .channel(NioServerSocketChannel.class)
+                .childHandler(new ChannelInitializer<NioSocketChannel>() {
+                    @Override
+                    protected void initChannel(NioSocketChannel nioSocketChannel) throws Exception {
+                        nioSocketChannel.pipeline().addLast(new StringDecoder());
+                        nioSocketChannel.pipeline().addLast(new SimpleChannelInboundHandler<String>() {
+                            @Override
+                            protected void channelRead0(ChannelHandlerContext channelHandlerContext, String s) throws Exception {
+                                System.out.println("content:" + s);
+                            }
+                        });
+                    }
+                })
+                .bind(8000);
+
+    }
+}
