0d699dd2e76f52e54c56e9fddbc3167cb53a9dff
cuihanze <cuihanze@meituan.com>
2019-09-26 17:48:46 +0800

    客户端 服务端  双向通信

--- src/main/java/com/cui/study/netty/im/commucationDemo/FirstClientHandler.java
+++ src/main/java/com/cui/study/netty/im/commucationDemo/FirstClientHandler.java
@@ -5,26 +5,22 @@ import io.netty.channel.ChannelHandlerContext;
 import io.netty.channel.ChannelInboundHandlerAdapter;
 
 import java.nio.charset.Charset;
+import java.util.Date;
 
 public class FirstClientHandler extends ChannelInboundHandlerAdapter {
-
     // 在客户端连接建立成功之后被调用
     @Override
     public void channelActive(ChannelHandlerContext ctx) throws Exception {
         // 获取数据
-        ByteBuf buf = getByteBuffer(ctx);
+        ByteBuf buf = Utils.getByteBuf(ctx, "世界，你好");
         // 写数据
         ctx.channel().writeAndFlush(buf);
     }
 
-    private ByteBuf getByteBuffer(ChannelHandlerContext ctx) {
-        // 获取二进制抽象 ByteBuf
-        ByteBuf buf = ctx.alloc().buffer();
-        // 准备数据，指定字符串的字符集为 utf-8
-        byte[] bytes = "世界，你好".getBytes(Charset.forName("utf-8"));
-        // 填充数据到 ByteBuf
-        buf.writeBytes(bytes);
-
-        return buf;
+    // 客户端读取服务端的数据
+    @Override
+    public void channelRead(ChannelHandlerContext ctx, Object msg) throws Exception {
+        ByteBuf byteBuf = (ByteBuf) msg;
+        System.out.println(new Date() + " : 客户端读到数据 -> " + byteBuf.toString(Charset.forName("utf-8")));
     }
 }
--- src/main/java/com/cui/study/netty/im/commucationDemo/FirstServerHandler.java
+++ src/main/java/com/cui/study/netty/im/commucationDemo/FirstServerHandler.java
@@ -12,7 +12,13 @@ import java.util.Date;
 public class FirstServerHandler extends ChannelInboundHandlerAdapter {
     @Override
     public void channelRead(ChannelHandlerContext ctx, Object msg) throws Exception {
+        // 服务端读取客户端的数据
         ByteBuf byteBuf = (ByteBuf) msg;
         System.out.println(new Date() + "：服务端读到数据 -> " + byteBuf.toString(Charset.forName("utf-8")));
+
+        // 服务器回写数据
+        System.out.println(new Date() + ": 服务端写出数据");
+        ByteBuf out = Utils.getByteBuf(ctx, "服务器回写数据：hello world");
+        ctx.channel().writeAndFlush(out);
     }
 }
