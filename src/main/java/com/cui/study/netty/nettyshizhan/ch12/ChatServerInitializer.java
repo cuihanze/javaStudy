75bd7527873374a502471312c38c2546646c9d9b
cuihanze <cuihanze@meituan.com>
2019-10-16 11:40:59 +0800

    netty websocket

--- src/main/java/com/cui/study/netty/nettyshizhan/ch12/ChatServerInitializer.java
+++ src/main/java/com/cui/study/netty/nettyshizhan/ch12/ChatServerInitializer.java
@@ -19,9 +19,9 @@ public class ChatServerInitializer extends ChannelInitializer<Channel> {    //1
     @Override
     protected void initChannel(Channel ch) throws Exception {            //2
         ChannelPipeline pipeline = ch.pipeline();
-        pipeline.addLast(new HttpServerCodec());
-        pipeline.addLast(new HttpObjectAggregator(64 * 1024));
-        pipeline.addLast(new ChunkedWriteHandler());
+        pipeline.addLast(new HttpServerCodec());// 将请求和应答消息解码为HTTP消息
+        pipeline.addLast(new HttpObjectAggregator(64 * 1024));// 将HTTP消息的多个部分合成一条完整的HTTP消息
+        pipeline.addLast(new ChunkedWriteHandler());// 向客户端发送HTML5文件
         pipeline.addLast(new HttpRequestHandler("/ws"));
         pipeline.addLast(new WebSocketServerProtocolHandler("/ws"));
         pipeline.addLast(new TextWebSocketFrameHandler(group));
